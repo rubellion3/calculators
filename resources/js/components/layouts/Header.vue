bbe3345650c6dffc85471ba37a626f30f5d43cee
Pannawat Choosang <pannawat.chs@gmail.com>
2020-10-27 03:22:50 +0700

    header

--- resources/js/components/layouts/Header.vue
+++ resources/js/components/layouts/Header.vue
@@ -1,5 +1,5 @@
 <template>
     <nav class="navbar sticky-top navbar-light bg-light shadow">
-        <a class="navbar-brand" href="#">Double Calculator</a>
+        <a class="navbar-brand" href="#"><h4>Double Calculator</h4> </a>
     </nav>
 </template>
